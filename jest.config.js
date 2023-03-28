ff53d7bcfa2afa71e68a38dfae1621b264081729
Muslim Zamukaev <abu.safiia2016@gmail.com>
2023-03-28 14:35:04 +0200

    fixed testing bug

--- jest.config.js
+++ jest.config.js
@@ -9,15 +9,6 @@ const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: [
-    "@testing-library/react/cleanup-after-each",
-    "@testing-library/jest-dom/extend-expect",
-  ],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
 };
 
 module.exports = createJestConfig(customJestConfig);

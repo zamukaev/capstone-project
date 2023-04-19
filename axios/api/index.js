ca074a5685325cbeaaf6da5156c8f6bcfbc53321
Muslim Zamukaev <abu.safiia2016@gmail.com>
2023-04-19 13:44:37 +0200

    add headers to axios

--- axios/api/index.js
+++ axios/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   withCredentials: true,
   baseURL: process.env.NEXT_PUBLIC_DOMAIN,
-  headers: {},
+  headers: { headers: { "Content-Type": "application/json" } },
 });
 
 class postsAPI {
